ffa1b7d8d320e393382deee37cca4f733469fecf
andrebayup14 <abayup1498@gmail.com>
2022-11-28 19:00:05 +0700

    Push Tank.cs

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //kebutuhan: tank bisa gerak dengan wasd tanpa transform.position
+    //public Vector3 limitAccel;
+    float speed = 5f;
+    Rigidbody rb;
+    //tank bisa rotate
+    //tank memiliki akselerasi dan maksimum nilai akselerasi dibatasi
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition((Vector3.right * speed * Time.deltaTime) + rb.position);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition((Vector3.left * speed * Time.deltaTime) + rb.position);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition((Vector3.back * speed * Time.deltaTime) + rb.position);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition((Vector3.forward * speed * Time.deltaTime) + rb.position);
+        }
+    }
+}
